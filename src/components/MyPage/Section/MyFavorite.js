9aebfdbc30e6a7c1bd2339add38ee77780cbc58c
[talkingOrange] <sonrisa-bonita@naver.com>
2022-07-26 15:14:50 +0900

    mypage_myfavorite

--- src/components/MyPage/Section/MyFavorite.js
+++ src/components/MyPage/Section/MyFavorite.js
@@ -24,16 +24,60 @@ function myFavorite() {
 
   return (
     <>
-      <BoardUl>
-        <ul className="boardTitle">
-          <li className="num">번호</li>
-          <li className="date">일자</li>
-          <li className="name">작성자</li>
-          <li className="title">글 제목</li>
-          <li className="content">본문 내용</li>
-        </ul>
-      </BoardUl>
-      <hr></hr>
+      <BoardUI>
+        <table>
+          <thead>
+            <tr>
+              <th>번호</th>
+              <th className="date">일자</th>
+              <th className="user">글쓴이</th>
+              <th>글 제목</th>
+              <th>본문 내용</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="num">1</td>
+              <td className="date">2022.07.24</td>
+              <td className="user">글쓴이</td>
+
+              <td className="title">뭐라뭐라 제목이올시다</td>
+              <td className="content">뭐라뭐라 내용이 올시다.</td>
+            </tr>
+            <tr>
+              <td className="num">2</td>
+              <td className="date">2022.07.24</td>
+              <td className="user">글쓴이</td>
+
+              <td className="title">뭐라뭐라 제목이올시다</td>
+              <td className="content">뭐라뭐라 내용이 올시다.</td>
+            </tr>
+            <tr>
+              <td className="num">3</td>
+              <td className="date">2022.07.24</td>
+              <td className="user">글쓴이</td>
+
+              <td className="title">뭐라뭐라 제목이올시다</td>
+              <td className="content">뭐라뭐라 내용이 올시다.</td>
+            </tr>
+            <tr>
+              <td className="num">4</td>
+              <td className="date">2022.07.24</td>
+              <td className="user">글쓴이</td>
+
+              <td className="title">뭐라뭐라 제목이올시다</td>
+              <td className="content">뭐라뭐라 내용이 올시다.</td>
+            </tr>
+            <tr>
+              <td className="num">5</td>
+              <td className="date">2022.07.24</td>
+              <td className="user">글쓴이올시다</td>
+              <td className="title">뭐라뭐라 제목이올시다</td>
+              <td className="content">뭐라뭐라 내용이 올시다.</td>
+            </tr>
+          </tbody>
+        </table>
+      </BoardUI>
       {myFavorites.length === 0 && (
         <NothingBox>
           <NothingAlert>좋아요한 게시글이 없습니다.</NothingAlert>
@@ -72,31 +116,53 @@ const NothingAlert = styled.p`
   color: #ff0200;
 `;
 
-const BoardUl = styled.ul`
+const BoardUI = styled.tbody`
   width: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  .boardTitle {
-    display: float;
-    padding: 7px;
-    font-size: 14px;
-    font-family: 'SCDream-bold';
-    font-weight: 100;
-  }
+  border-collapse: separate;
+  border-spacing: 0 10px;
+
   .num {
-    margin-right: 7%;
+    width: 5%;
+    text-align: center;
   }
   .date {
-    margin-right: 10%;
+    width: 5%;
+  }
+  .user {
+    text-align: center;
+
+    width: 10%;
   }
   .title {
-    margin-right: 20%;
+    width: 25%;
+    text-align: center;
   }
-  .content {
-    margin-right: 20%;
+
+  table {
+    width: 100%;
+    border-collapse: collapse;
+    font-size: 14px;
+    border-top: 1px solid #444444;
+    margin-top: 7px;
   }
-  .name {
-    margin-right: 10%;
+
+  td {
+    text-aligh: left;
+    padding: 10px;
+    border-bottom: 1px solid #444444;
+    margin-left: 10px;
+  }
+  th {
+    padding: 15px;
+    border-bottom: 1px solid #444444;
+    margin-left: 10px;
+  }
+
+  thead {
+    font-family: 'SCDream-bold';
+    font-weight: 100;
   }
 `;
