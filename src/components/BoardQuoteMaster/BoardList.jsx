1a7a07fbe72b60e69189a482541503212897835e
hanit3 <esolbpmj3@gmail.com>
2022-07-27 20:56:45 +0900

    boardlist css

--- src/components/BoardQuoteMaster/BoardList.jsx
+++ src/components/BoardQuoteMaster/BoardList.jsx
@@ -31,49 +31,100 @@ function BoardList(history) {
         <BoardListTextBox></BoardListTextBox>
         <WriteButton></WriteButton>
       </BoardTopBlock>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>번호</th>
-            <th>제목</th>
-            <th>작성자</th>
-            <th>작성일</th>
-            <th>조회수</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>
-              <Link
-                style={{ textDecoration: 'none', color: 'black' }}
-                to="/quotemaster/board/quotkaId"
-              >
-                New Title Test
-              </Link>
-            </td>
-            <td>쿼억하</td>
-            <td>2022-03-19</td>
-            <td>32</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>게시글2</td>
-            <td>aaaaa</td>
-            <td>2022-03-19</td>
-            <td>18</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>게시글2</td>
-            <td>bbbbb</td>
-            <td>2022-03-19</td>
-            <td>21</td>
-          </tr>
-        </tbody>
-      </Table>
+      <BoardTable>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>번호</th>
+              <th>제목</th>
+              <th>작성자</th>
+              <th>작성일</th>
+              <th>조회수</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>
+                <Link
+                  style={{ textDecoration: 'none', color: 'black' }}
+                  to="/quotemaster/board/quotkaId"
+                >
+                  New Title Test
+                </Link>
+              </td>
+              <td>쿼억하</td>
+              <td>2022-03-19</td>
+              <td>32</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>게시글2</td>
+              <td>aaaaa</td>
+              <td>2022-03-19</td>
+              <td>18</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>게시글2</td>
+              <td>bbbbb</td>
+              <td>2022-03-19</td>
+              <td>21</td>
+            </tr>
+          </tbody>
+        </Table>
+      </BoardTable>
     </>
   );
 }
 
 export default BoardList;
+
+const BoardTable = styled.table`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  border-collapse: separate;
+  border-spacing: 0 10px;
+
+  .num {
+    width: 5%;
+    text-align: center;
+  }
+  .date {
+    width: 7%;
+  }
+  .title {
+    width: 25%;
+    text-align: center;
+  }
+  .content {
+    width: 63%;
+  }
+
+  table {
+    width: 100%;
+    border-collapse: collapse;
+    font-size: 14px;
+    border-top: 1px solid #444444;
+    margin-top: 7px;
+  }
+
+  td {
+    text-aligh: left;
+    padding: 10px;
+    border-bottom: 1px solid #444444;
+    margin-left: 10px;
+  }
+  th {
+    padding: 15px;
+    border-bottom: 1px solid #444444;
+    margin-left: 10px;
+  }
+
+  thead {
+    font-family: 'SCDream-bold';
+    font-weight: 100;
+  }
+`;
